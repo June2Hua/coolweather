941c891caada100f61b693194b13ce682a2cea8c
zhuangjinhua <zhuang@gmail.com>
2018-11-23 22:30:07 +0800

    完成JSON解析所需要的类

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,12 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2018/11/23.
+ */
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String api;
+        public String pm25;
+    }
+}
